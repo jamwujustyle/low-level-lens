ba51a6973d83955259847b2edf78675d68fe8ff9
codeBuddha <jamwujustyle@gmail.com>
2026-04-24 17:31:36 +0500

    add lexer test suite

---
+++ compiler/lexer_test.go
@@ -0,0 +1,38 @@
+package compiler
+
+import "testing"
+
+func TestNextToken(t *testing.T) {
+	input := `5 PLUs (10 * X) - V`
+
+	tests:= []struct{
+		expectedType 	TokenType
+		expectedLiteral string
+	}{
+		{TokenNumber, "5"},
+		{TokenPlus, "+"},
+		{TokenLParen, "("},
+		{TokenNumber, "10"},
+		{TokenStar, "X"},
+		{TokenRParen, ")"},
+		{TokenMinus, "-"},
+		{TokenNumber, "v"},
+		{TokenEOF, ""},
+	}
+	l:= NewLexer(input)
+
+	for i, tt:= range tests {
+		tok:= l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokenType wrong. expected=%d, got=%d",
+				i, tt.expectedType, tok.Type,
+			)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal,
+			)
+		}
+	}
+}
