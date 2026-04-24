b70f35a0d0289fa7fafd2bfd360e077e1243fd0c
codeBuddha <jamwujustyle@gmail.com>
2026-04-24 16:53:53 +0500

    define token/lexer & complete lexer scaffolding

---
+++ compiler/lexer.go
@@ -0,0 +1,128 @@
+package compiler
+
+import "strings"
+type TokenType int
+const (
+	TokenEOF TokenType = iota
+	TokenNumber
+	TokenPlus
+	TokenMinus
+	TokenStar
+	TokenSlash
+	TokenLParen
+	TokenRParen
+)
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+
+type Lexer struct {
+	input string
+	position int
+	readPosition int
+	ch byte
+}
+
+func lookupIdent(ident string) TokenType {
+	keywords:= map[string]TokenType{
+		"plus": TokenPlus,
+		"minus": TokenMinus,
+		"times": TokenStar,
+		"divided": TokenSlash,
+		"x": TokenNumber,
+		"v": TokenNumber,
+		"i": TokenNumber,
+	}
+	if tok, ok:= keywords[strings.ToLower(ident)]; ok {
+		return tok
+	}
+	return TokenEOF
+}
+
+func NewLexer(input string) *Lexer {
+	l:= &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	}else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition +=1
+}
+
+func newToken(tt TokenType, ch byte) Token{
+	return Token{Type: tt, Literal: string(ch)}
+}
+func (l *Lexer) skipWhitespaces() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+func (l *Lexer) NextToken() Token {
+	var tok Token
+	l.skipWhitespaces()
+	switch l.ch {
+	case '+':
+		tok = newToken(TokenPlus, l.ch)
+	case '-':
+		tok = newToken(TokenMinus, l.ch)
+	case '*':
+		tok = newToken(TokenStar, l.ch)
+	case '/':
+		tok = newToken(TokenSlash, l.ch)
+	case '(':
+		tok = newToken(TokenLParen, l.ch)
+	case ')':
+		tok = newToken(TokenRParen, l.ch)
+	case 0:
+		tok.Type = TokenEOF
+		tok.Literal = ""
+	default:
+		if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = TokenNumber
+			return tok
+		}else if isLetter(l.ch) {
+				tok.Literal = l.readIdentifier()
+				tok.Type = lookupIdent(tok.Literal)
+				return tok
+		} else {
+			tok = newToken(TokenEOF, l.ch)
+		}
+
+	}
+	l.readChar()
+	return tok
+}
+
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+func (l *Lexer) readNumber() string {
+	pos := l.position
+
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[pos:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z'
+}
+func (l *Lexer) readIdentifier() string {
+	pos:= l.position
+
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[pos:l.position]
+}
