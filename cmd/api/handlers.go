753eadd63c51c9b37e907274d2cd2ea3979aa9bd
codeBuddha <jamwujustyle@gmail.com>
2026-04-29 17:46:03 +0500

    add step endpoint

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	c "github.com/jamwujustyle/low-level-lens/compiler"
@@ -10,6 +11,7 @@ import (
 
 func handlePing(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	if err := json.NewEncoder(w).Encode(map[string]string{"message": "ping"}); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -19,6 +21,7 @@ func handlePing(w http.ResponseWriter, r *http.Request) {
 func handleCompile(w http.ResponseWriter, r *http.Request) {
 	var req CompileRequest
 	var res CompileResponse
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -43,6 +46,27 @@ func handleCompile(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	res = CompileResponse{Assembly: comp.Assembly}
+
+	if err := json.NewEncoder(w).Encode(&res); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+}
+
+func handleStep(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	if gCPU == nil {
+		err := errors.New("CPU not initialized")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	gCPU.Step()
+
+	res := StepResponse{
+		Registers: gCPU.Registers,
+		PC:        gCPU.PC,
+		Halt:      gCPU.Halt,
+	}
 
 	if err := json.NewEncoder(w).Encode(&res); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
--- cmd/api/models.go
+++ cmd/api/models.go
@@ -8,3 +8,9 @@ type CompileResponse struct {
 	Message  string   `json:"message"`
 	Assembly []string `json:"assembly"`
 }
+
+type StepResponse struct {
+	Registers [4]int32 `json:"registers"`
+	PC        int      `json:"pc"`
+	Halt      bool     `json:"halt"`
+}
